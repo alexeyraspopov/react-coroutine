7fb1864a9052fb7332b5128dc0bd653cc61062d4
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-09-11 01:33:45 +0300

    implement base unit test

--- tests/Coroutine.test.js
+++ tests/Coroutine.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Renderer from 'react-test-renderer';
 import Coroutine from '../src/Coroutine';
 
-describe('Coroutine', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy();
+describe('Coroutine', async () => {
+  it('should render empty body until coroutine is resolved', async () => {
+    const TestComponent = Coroutine.create(async () => <p>test</p>);
+    const tree = Renderer.create(<TestComponent />);
+    const initial = Renderer.create(<noscript />);
+    const success = Renderer.create(<p>test</p>);
+
+    expect(tree.toJSON()).toEqual(initial.toJSON());
+    await true;
+    expect(tree.toJSON()).toEqual(success.toJSON());
   });
 });
