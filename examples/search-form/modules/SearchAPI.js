e2da286575f925e02e26abc3d0e5aa56ab2ae8cb
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-27 00:17:01 +0300

    search-form: add code annotations

--- examples/search-form/modules/SearchAPI.js
+++ examples/search-form/modules/SearchAPI.js
@@ -2,16 +2,20 @@ import debounce from './Debounce';
 
 class Search {
   constructor() {
+    /* The method is debounced for the sake of not doing unnecessary HTTP requests. */
     this.retrieve = debounce(this.retrieve.bind(this));
   }
 
   async retrieve(query) {
+    /* npms.io search API is used in this example. Good stuff.*/
     const response = await fetch(`https://api.npms.io/v2/search?from=0&size=25&q=${query}`);
 
     if (response.ok) {
       return response.json();
     }
 
+    /* If API returns some weird stuff and not 2xx, convert it to error and show
+       on the screen. */
     throw new Error(await response.text());
   }
 }
--- examples/search-form/modules/SearchForm.js
+++ examples/search-form/modules/SearchForm.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Coroutine from 'react-coroutine';
 import SearchAPI from './SearchAPI';
 
+/* A coroutine becomes a React component via this wrapper. */
 export default Coroutine.create(SearchForm);
 
+/* Async generator is used as a component that represents a stateful component
+   with search results. The same rules are applicable as to functional component.
+   The main difference comparing to plain functional component is that async generator
+   yields particular UI state and then performs additional actions (awaitg for data)
+   to create and yield new UI state. */
+/* If you don't know what the thing is async generator, check the TC39 proposal:
+   https://github.com/tc39/proposal-async-iteration#async-generator-functions */
 async function* SearchForm({ query }) {
+  /* Not really important. There is nothing to show if query is empty. */
   if (query.length === 0) return null;
 
+  /* This call does not finish the execution of the component. It just provides a
+     state of UI and then doing another stuff. */
   yield <p>Searching {query}...</p>;
 
   try {
+    /* This piece is the same as with async functions. Some data is fetched and
+       used with another plain functional component. */
     const { results } = await SearchAPI.retrieve(query);
     return <SearchResults results={results} />;
   } catch (error) {
