f963de75ad8b2c2db0de1d1233a3b037fb75e640
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-26 19:06:26 +0300

    search-form: implement primitive UI

--- examples/search-form/modules/SearchForm.js
+++ examples/search-form/modules/SearchForm.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Coroutine from '../../../modules/Coroutine';
+import SearchAPI from './SearchAPI';
 
 export default Coroutine.create(SearchForm);
 
 async function* SearchForm({ query }) {
   yield <p>Searching "{query}"</p>;
+  try {
+    const results = await SearchAPI.retrieve(query);
+    return <SearchResults results={results} />;
+  } catch (error) {
+    return <p>Oops...</p>;
+  }
+}
+
+function SearchResults({ results }) {
+  if (results.length === 0) {
+    return <p>No results</p>;
+  }
+
+  return (
+    <ul>
+      {results.map((result) => (
+        <li key={result.package.name}>{result.package.name} ({result.score.final})</li>
+      ))}
+    </ul>
+  );
 }
