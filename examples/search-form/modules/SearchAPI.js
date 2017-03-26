c66377ea5e7d84ca4b78ea4a4088cccdd4af53b1
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-26 22:54:54 +0300

    search api: throw response as an error

--- examples/search-form/modules/SearchAPI.js
+++ examples/search-form/modules/SearchAPI.js
@@ -6,10 +6,13 @@ class Search {
   }
 
   async retrieve(query) {
-    if (!query) return [];
     const response = await fetch(`https://api.npms.io/v2/search?from=0&size=25&q=${query}`);
-    const { results, total } = await response.json();
-    return results;
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(await response.text());
   }
 }
 
