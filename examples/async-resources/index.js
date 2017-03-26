9473535416adbbcaff1393f10163083f4b56dce6
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-03-26 14:03:53 +0300

    remove pending files

--- examples/async-resources/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PokemonList from './modules/PokemonList.react';
-
-ReactDOM.render(<PokemonList />, document.querySelector('main'));
