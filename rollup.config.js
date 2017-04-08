4e8aa1a7d2244acd3f7d317eef63da3e78ec0d40
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-08 10:28:18 +0300

    move build config to rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
 import babel from 'rollup-plugin-babel';
 
+const config = {
+  babelrc: false,
+  presets: [
+    ['es2015', { modules: false, loose: true }],
+  ],
+  plugins: ['external-helpers']
+}
+
 export default {
-  plugins: [babel()]
+  plugins: [babel(config)]
 }
