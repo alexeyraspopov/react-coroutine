7d1e16ffadfdbfda9f838a8f495732325654bcc6
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-06-22 02:13:41 +0300

    make Coroutine a Component; allow subclassing

--- modules/Coroutine.js
+++ modules/Coroutine.js
@@ -1,4 +1,99 @@
-import create from './CoroutineFactory';
-import render from './CoroutineRender';
+import { Component } from 'react';
+import isEqual from 'shallowequal';
 
-export default { create, render };
+export default class Coroutine extends Component {
+  static create(asyncFn) {
+    return class AsyncComponent extends Coroutine {
+      static get contextTypes() {
+        return asyncFn.contextTypes;
+      }
+
+      static get displayName() {
+        return `Coroutine(${asyncFn.name})`;
+      }
+
+      observe() {
+        return asyncFn(this.props, this.context);
+      }
+
+      render() {
+        return this.state.data;
+      }
+    }
+  }
+
+  static get displayName() {
+    return `Coroutine(${this.name})`;
+  }
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { data: null };
+    this.iterator = null;
+    this.forceUpdateHelper = this.forceUpdate.bind(this);
+    this.isComponentMounted = false;
+  }
+
+  observe() {
+    throw new Error('Coroutine::observe should be implemented by a subclass');
+  }
+
+  forceUpdate(props) {
+    const asyncBody = this.observe();
+
+    this.iterator = asyncBody;
+
+    if (typeof asyncBody.then === 'function') {
+      asyncBody.then(data => {
+        if (this.isComponentMounted && this.iterator === asyncBody) {
+          this.setState(() => ({ data }));
+        }
+      });
+    } else {
+      const getNextBody = () => {
+        this.iterator.next().then((data) => {
+          if (!this.isComponentMounted || this.iterator !== asyncBody) {
+            return;
+          }
+
+          if (data.value !== undefined) {
+            this.setState(() => ({ data: data.value }));
+          }
+
+          return !data.done && getNextBody();
+        });
+      };
+
+      getNextBody();
+    }
+  }
+
+  componentDidMount() {
+    this.isComponentMounted = true;
+    return this.forceUpdate(this.props);
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    if (!isEqual(this.props, nextProps)) {
+      if (this.iterator && this.iterator.return) {
+        this.iterator.return();
+      }
+
+      if (this.isComponentMounted) {
+        this.forceUpdate(nextProps);
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.iterator && this.iterator.return) {
+      this.iterator.return();
+    }
+
+    this.isComponentMounted = false;
+  }
+
+  render() {
+    throw new Error('Coroutine::render should be implemented by a subclass');
+  }
+}
--- tests/Coroutine.test.js
+++ tests/Coroutine.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Renderer from 'react-test-renderer';
 import Coroutine from '../modules/Coroutine';
 
-describe('Coroutine Factory', async () => {
+describe('Coroutine', async () => {
   it('should render empty body until coroutine is resolved', async () => {
     async function render() {
       return <p>test</p>;
@@ -14,22 +14,6 @@ describe('Coroutine Factory', async () => {
     expect(tree.toJSON()).toEqual(null);
 
     const success = await Renderer.create(<p>test</p>);
-    expect(tree.toJSON()).toEqual(success.toJSON());
-  });
-
-  it('should pass initial information', async () => {
-    function getVariables() {
-      return { number: 13 };
-    }
-
-    async function render({ number }) {
-      return <p>{ number }</p>;
-    }
-
-    const TestComponent = Coroutine.create(render, getVariables);
-    const tree = Renderer.create(<TestComponent />);
-
-    const success = await Renderer.create(<p>{13}</p>);
     expect(tree.toJSON()).toEqual(success.toJSON());
   });
 
@@ -48,21 +32,5 @@ describe('Coroutine Factory', async () => {
 
     const second = await Renderer.create(<p>Done!</p>);
     expect(tree.toJSON()).toEqual(second.toJSON());
-  });
-
-  it('should allow placeholder replacement', async () => {
-    function getVariables() {
-      return { placeholder: <p>hello</p> };
-    }
-
-    async function render() {
-      return <p>test</p>;
-    }
-
-    const TestComponent = Coroutine.create(render, getVariables);
-    const tree = Renderer.create(<TestComponent />);
-
-    const placeholder = Renderer.create(<p>hello</p>);
-    expect(tree.toJSON()).toEqual(placeholder.toJSON());
   });
 });
