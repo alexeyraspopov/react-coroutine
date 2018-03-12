987d466b1e8b08af58254b864f6a3d8db81e8492
Alexey Raspopov <alexey.raspopov@datarobot.com>
2018-03-12 00:39:57 -0400

    use multi output rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
 import babel from 'rollup-plugin-babel';
 
-const config = {
+let config = {
   babelrc: false,
   presets: [
-    ['es2015', { modules: false, loose: true }],
+    ['env', { modules: false, loose: true }],
   ],
   plugins: ['external-helpers']
 };
 
-export default {
-  plugins: [babel(config)]
-};
+export default [
+  {
+    input: 'modules/Coroutine.js',
+    output: {
+      file: 'build/react-coroutine.js',
+      format: 'cjs',
+    },
+    plugins: [babel(config)],
+    external: ['react'],
+  },
+  {
+    input: 'modules/Coroutine.js',
+    output: {
+      file: 'build/react-coroutine.module.js',
+      format: 'es',
+    },
+    plugins: [babel(config)],
+    external: ['react'],
+  },
+];
